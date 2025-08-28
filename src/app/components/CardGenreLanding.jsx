4ca1035c53477f790d803377a067789da765c051
Ingryd <ingrydcduarte@gmail.com>
2025-08-27 22:57:10 -0300

    feat: genre card landing generic

--- src/app/components/CardGenreLanding.jsx
+++ src/app/components/CardGenreLanding.jsx
@@ -1,13 +1,17 @@
 import "../styles/CardGenreLanding.css";
+import "../animations/microBalance.css";
 
-const CardGenreLanding = () => {
-    return (
-        <div className="card-genre-container">
-            <div className="halftone-genre-effect">
-                <h2 className="title-genre">Post-Indie-MPB</h2>
-            </div>
-        </div>
-    )
-}
+const CardGenreLanding = ({ genre, imageUrl }) => {
+  return (
+    <div
+      className="card-genre-container"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <div className="halftone-genre-effect">
+        <h2 className="title-genre">{genre}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default CardGenreLanding;
