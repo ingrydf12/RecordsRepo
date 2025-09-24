a10e3d187e274498abcc3267961c8f7ea0989841
Tonny Kimanthi <kimaxhanthony9@gmail.com>
2025-09-24 13:41:41 +0300

    Add responsive header with collapsible menu

--- src/app/components/LandingHeader.jsx
+++ src/app/components/LandingHeader.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Image from "next/image";
 import "../styles/HeaderLanding.css";
 import Link from "next/link";
 
 const HeaderHome = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false);
   return (
-    <header className="header">
+    <header className={`header ${navIsOpen ? "pad-bottom" : ""}`}>
       <Link href="/" className="logotipo">
         <Image
           src="/logoAberto.png"
@@ -16,8 +18,45 @@ const HeaderHome = () => {
         <p>RecordsRepo</p>
       </Link>
 
+      {!navIsOpen ? (
+        <button className="menu-btn" onClick={() => setNavIsOpen(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+      ) : (
+        <button
+          className="menu-btn close-btn"
+          onClick={() => setNavIsOpen(false)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      )}
+
       <nav className="nav">
-        <ul className="header-links">
+        <ul className={`header-links ${navIsOpen ? "show" : "hide"}`}>
           <li>
             <a href="#about">Sobre</a>
           </li>
